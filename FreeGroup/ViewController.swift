911f8a40ab7c1b3b2f77d08c0bf0d27727250126
Macbook Pro A1990 <macbookproa1990@FCS.local>
2018-12-05 14:43:33 -0600

    Initial Commit

---
+++ FreeGroup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeGroup
+//
+//  Created by Macbook Pro A1990 on 12/5/18.
+//  Copyright © 2018 Ayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
