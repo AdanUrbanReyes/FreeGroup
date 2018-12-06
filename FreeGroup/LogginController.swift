ff896d37e9bd2b641d1b3eadc8a56d27d205882a
Macbook Pro A1990 <macbookproa1990@FCS.local>
2018-12-06 11:15:08 -0600

    Practice 2 finished, obiusly it could be better

---
+++ FreeGroup/LogginController.swift
@@ -0,0 +1,30 @@
+//
+//  LogginController.swift
+//  FreeGroup
+//
+//  Created by Macbook Pro A1990 on 12/6/18.
+//  Copyright © 2018 Ayan. All rights reserved.
+//
+
+import UIKit
+
+class LogginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
